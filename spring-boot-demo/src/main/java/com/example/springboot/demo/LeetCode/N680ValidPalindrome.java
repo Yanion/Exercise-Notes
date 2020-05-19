c4103d250c293977695debe39bd41ff8ffa61187
maning <maning04@cncic.chemchina.com>
2020-05-19 14:42:46 +0800

    创建刷题笔记项目

---
+++ spring-boot-demo/src/main/java/com/example/springboot/demo/LeetCode/N680ValidPalindrome.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demo.LeetCode;
+
+public class N680ValidPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println("2222222");
+    }
+    public boolean validPalindrome(String s) {
+
+
+        return false;
+    }
+}
